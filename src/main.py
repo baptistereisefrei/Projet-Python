06a208b67365416825344aae6ec91c63ac28705d
Margaux <dev.agence.thrive@gmail.com>
2023-12-05 17:17:18 +0100

    [ADD] get popular film via TMDB API

---
+++ src/main.py
@@ -0,0 +1,42 @@
+import requests
+from rich.pretty import pprint
+
+def parseJson(jsonFile):
+    if (jsonFile == None):
+        return None
+    
+    moviesArray = []
+
+    for i in jsonFile['results']:
+        movieArray = {
+            'title': i['title'],
+            'overview': i['overview'],
+            'release_date': i['release_date'],
+        }
+        moviesArray.append(movieArray)
+    return moviesArray
+
+def requestApi(findUrl):
+    headers = {
+        "accept": "application/json",
+    }
+
+    urlPattern = 'https://api.themoviedb.org/3/movie/'
+    apiKey = '?api_key=10cc4880f99db6dbc89c50638b3663cd'
+
+    response = requests.get(urlPattern + findUrl + apiKey, headers=headers)
+
+    return response
+
+def openAccessTokenFile():
+    with open("accessToken.txt") as file:
+        data = file.read()
+        return data
+
+def mainProgram():
+    response = requestApi('popular')
+    moviesArray = parseJson(response.json())
+    pprint(moviesArray)
+
+if __name__ == "__main__":
+    mainProgram()
